bb7443ac39abba327bf4469988b7468d7abb104b
datadog-bits-staging <264369727+datadog-bits-staging@users.noreply.github.com>
2026-08-19 19:54:40 +0000

    Add pink Sarah app
    Co-authored-by: setnilson <110850377+setnilson@users.noreply.github.com>

---
+++ apps/sarah-s-app-wed-aug-19-3-51-17-pm/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <main className="sarah-app" aria-labelledby="sarah-title">
+            <h1 id="sarah-title">sarah</h1>
+        </main>
+    );
+}
+
+export default App;
